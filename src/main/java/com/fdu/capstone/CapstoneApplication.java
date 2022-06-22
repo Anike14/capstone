19fbfc9e2f3e5662c5bfa4b7f279e3c4d9f5718a
anike <anike14@hotmail.com>
2022-06-22 15:21:18 -0700

    add componentScan and generic header in the project

--- src/main/java/com/fdu/capstone/CapstoneApplication.java
+++ src/main/java/com/fdu/capstone/CapstoneApplication.java
@@ -2,8 +2,10 @@ package com.fdu.capstone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.fdu.capstone.viewcontrollers","com.fdu.capstone.models"})
 public class CapstoneApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,3 @@
+$("#header")[0].addEventListener("headerRendered", function () {
+	 $("#home-tab")[0].classList.add("active");
+});
